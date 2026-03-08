de0e1bf47c4819b98d14a2879321f5e01cef2add
Pwere-02 <panuhawere143@gmail.com>
2026-03-07 20:55:25 -0500

    Refactor lidar_polar_to_xy function formatting

--- Software/ESP32/test/main/lidar_xy.c
+++ Software/ESP32/test/main/lidar_xy.c
@@ -4,7 +4,6 @@
 #include <stddef.h>
 
 #ifndef M_PI
-#define M_PI 3.14159265358979323846
 #endif
 
 bool lidar_polar_to_xy(const rplidar_point_t *in, lidar_xy_point_t *out)
@@ -34,3 +33,4 @@ bool lidar_polar_to_xy(const rplidar_point_t *in, lidar_xy_point_t *out)
 
     return true;
 }
+
